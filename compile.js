ca61e69b5ceac13a4efea604776236c930f5d603
Aytaditya <adityaaryan531@gmail.com>
2024-04-18 00:14:05 +0530

    deploy and compile code

--- compile.js
+++ compile.js
@@ -1,1 +1,30 @@
-// initial setup of compile.js
+const path=require('path') // path module is used to get the path of the file
+const fs=require('fs')  // fs module is used to read the file
+const solc=require('solc') // solc module is used to compile the solidity code
+
+const contractPath=path.resolve(__dirname,'contracts','Lottery.sol') // path of the file
+const source=fs.readFileSync(contractPath,'utf8') 
+
+const input={
+    language: 'Solidity',
+    sources: {
+        'Lottery.sol': {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+const output=JSON.parse(solc.compile(JSON.stringify(input))) // compile the solidity code
+const contract=output.contracts['Lottery.sol']['Lottery'] 
+console.log(contract)
+
+module.exports={
+    abi: contract.abi,
+    bytecode: contract.evm.bytecode.object
+}
--- deploy.js
+++ deploy.js
@@ -1,1 +1,23 @@
-// initial setup for deploy.js
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const Web3 = require('web3');
+const {abi,bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    "rotate valley average list misery tip soda fringe human wage area table","https://linea-sepolia.infura.io/v3/4a0c53a5e4d34ddfa9725982d54c25b5"
+)
+
+const web3 = new Web3(provider); // instance of web3
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: bytecode })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    console.log('Contract deployed to', result.options.address);
+};
+
+deploy(); // call the deploy function
